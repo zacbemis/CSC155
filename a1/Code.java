82b0e7674c77277823001929b514fdec43f4e3f5
zacb <zac.bemis916@gmail.com>
2026-02-15 16:32:08 -0800

    moving with info

--- a1/Code.java
+++ a1/Code.java
@@ -14,7 +14,9 @@ public class Code extends JFrame implements GLEventListener {
     private int vao[] = new int[1];
 
     private float xTriangle = 0.0f;
-    private float incTriangle = 0.01f;
+    private float speedTriangle = 0.5f;
+    private int directionTriangle = 1;
+    private long lastTimeMs = 0;
 
     public Code() {
         setTitle("A1");
@@ -29,6 +31,11 @@ public class Code extends JFrame implements GLEventListener {
 
     public void init(GLAutoDrawable drawable) {
         GL4 gl = (GL4) GLContext.getCurrentGL();
+        System.out.println("OpenGL version:  " + gl.glGetString(GL_VERSION));
+        System.out.println("OpenGL vendor:   " + gl.glGetString(GL_VENDOR));
+        System.out.println("OpenGL renderer: " + gl.glGetString(GL_RENDERER));
+        System.out.println("JOGL version:    " + GL4.class.getPackage().getImplementationVersion());
+        System.out.println("Java version:    " + System.getProperty("java.version"));
         renderingProgram = Utils.createShaderProgram("base.vert", "base.frag");
         gl.glGenVertexArrays(vao.length, vao, 0);
         gl.glBindVertexArray(vao[0]);
@@ -40,12 +47,15 @@ public class Code extends JFrame implements GLEventListener {
 		gl.glClear(GL_DEPTH_BUFFER_BIT);
 		gl.glUseProgram(renderingProgram);
 
-		xTriangle += incTriangle;
-		if (xTriangle > 1.0f) incTriangle = -incTriangle;
-		if (xTriangle < -1.0f) incTriangle = -incTriangle;
+		long now = System.currentTimeMillis();
+		float dt = (lastTimeMs == 0) ? 0 : (now - lastTimeMs) / 1000f;
+		lastTimeMs = now;
+
+		xTriangle += directionTriangle * speedTriangle * dt;
+		if (xTriangle > 1.0f)  { xTriangle = 1.0f;  directionTriangle = -1; }
+		if (xTriangle < -1.0f) { xTriangle = -1.0f; directionTriangle = 1; }
 		int offsetLoc = gl.glGetUniformLocation(renderingProgram, "offset");
 		gl.glProgramUniform1f(renderingProgram, offsetLoc, xTriangle);
-
 		gl.glDrawArrays(GL_TRIANGLES,0,3);
     }
 
