93740c86af0d19906df1c50c78afce948e6574dd
zacb <zac.bemis916@gmail.com>
2026-02-15 16:07:54 -0800

    boing

--- a1/Code.java
+++ a1/Code.java
@@ -6,11 +6,15 @@ import static com.jogamp.opengl.GL4.*;
 import com.jogamp.opengl.*;
 import com.jogamp.opengl.awt.GLCanvas;
 import com.jogamp.opengl.GLContext;
+import com.jogamp.opengl.util.*;
 
 public class Code extends JFrame implements GLEventListener {
     private GLCanvas myCanvas;
     private int renderingProgram;
     private int vao[] = new int[1];
+
+    private float xTriangle = 0.0f;
+    private float incTriangle = 0.01f;
 
     public Code() {
         setTitle("A1");
@@ -19,6 +23,8 @@ public class Code extends JFrame implements GLEventListener {
         myCanvas.addGLEventListener(this);
         this.add(myCanvas);
         this.setVisible(true);
+        Animator animator = new Animator(myCanvas);
+        animator.start();
     }
 
     public void init(GLAutoDrawable drawable) {
@@ -30,8 +36,17 @@ public class Code extends JFrame implements GLEventListener {
 
     public void display(GLAutoDrawable drawable) {
         GL4 gl = (GL4) GLContext.getCurrentGL();
-        gl.glUseProgram(renderingProgram);
-        gl.glDrawArrays(GL_TRIANGLES, 0, 3);
+		gl.glClear(GL_COLOR_BUFFER_BIT);
+		gl.glClear(GL_DEPTH_BUFFER_BIT);
+		gl.glUseProgram(renderingProgram);
+
+		xTriangle += incTriangle;
+		if (xTriangle > 1.0f) incTriangle = -incTriangle;
+		if (xTriangle < -1.0f) incTriangle = -incTriangle;
+		int offsetLoc = gl.glGetUniformLocation(renderingProgram, "offset");
+		gl.glProgramUniform1f(renderingProgram, offsetLoc, xTriangle);
+
+		gl.glDrawArrays(GL_TRIANGLES,0,3);
     }
 
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
--- a1/base.vert
+++ a1/base.vert
@@ -1,9 +1,12 @@
 #version 430
+
+uniform float offset;
+
 void main(void) {
     const vec4 vertices[3] = vec4[3](
-        vec4( 0.0,  0.5, 0.0, 1.0),
-        vec4(-0.1, -0.5, 0.0, 1.0),
-        vec4( 0.1, -0.5, 0.0, 1.0)
+        vec4( 0.0+offset,  0.5, 0.0, 1.0),
+        vec4(-0.1+offset, -0.5, 0.0, 1.0),
+        vec4( 0.1+offset, -0.5, 0.0, 1.0)
     );
     gl_Position = vertices[gl_VertexID];
 }
