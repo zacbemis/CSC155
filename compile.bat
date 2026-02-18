a90729a2dfa04c033d72510956eb4260b896148a
zacb <zac.bemis916@gmail.com>
2026-02-18 12:05:24 -0800

    .

--- compile.bat
+++ compile.bat
@@ -1,8 +1,1 @@
-@echo off
-setlocal
-
-cd /d %~dp0
-
-set CP=.;javagaming\jogl\jogamp-fat.jar;javagaming\joml\joml-1.10.7.jar
-
-javac -cp "%CP%" a1\*.java javagaming\utils\Utils.java
+javac a1/Code.java
--- run.bat
+++ run.bat
@@ -1,8 +1,1 @@
-@echo off
-setlocal
-
-cd /d %~dp0
-
-set CP=.;javagaming\jogl\jogamp-fat.jar;javagaming\joml\joml-1.10.7.jar
-
-java -cp "%CP%" --add-exports java.base/java.lang=ALL-UNNAMED --add-exports java.desktop/sun.awt=ALL-UNNAMED --add-exports java.desktop/sun.java2d=ALL-UNNAMED -Dsun.java2d.d3d=false -Dsun.java2d.uiScale=1 a1.Code
+java --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.desktop/sun.awt=ALL-UNNAMED --add-opens java.desktop/sun.java2d=ALL-UNNAMED --add-opens java.desktop/sun.awt.windows=ALL-UNNAMED -Dsun.java2d.d3d=false -Dsun.java2d.uiScale=1 code.Code
